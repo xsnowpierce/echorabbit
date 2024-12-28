0594b22a92e694e4241fa32d2f72c91d1879a15e
Snow Pierce <snowysnowypierce@gmail.com>
2024-12-28 16:55:57 -0500

    28/12/2024

--- src/main/java/snow/pierce/Main.java
+++ src/main/java/snow/pierce/Main.java
@@ -3,8 +3,8 @@ package snow.pierce;
 import snow.pierce.Renderer.Window;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Window window = Window.get();
         window.run();
     }
